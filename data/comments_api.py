5b583615b5dc5622e9c7f0db44cf4dd08bc75c4d
egor-nak <67370877+egor-nak@users.noreply.github.com>
2021-04-19 03:04:55 +0300

    Update comments_api.py

--- data/comments_api.py
+++ data/comments_api.py
@@ -1,7 +1,11 @@
 import flask
-from flask import jsonify, request, redirect
+from flask import jsonify, request, redirect, render_template
 from . import db_session
 from .comments import News
+from flask_login import current_user
+from flask_login import LoginManager, login_user, login_required, logout_user
+from .login_form import LoginForm
+from .users import User
 from flask_login import current_user
 
 blueprint = flask.Blueprint(
@@ -9,6 +13,30 @@ blueprint = flask.Blueprint(
     __name__,
     template_folder='templates'
 )
+
+namecur = ''
+
+
+@blueprint.route('/api/login', methods=['GET', 'POST'])
+def login():
+    global namecur
+    # print(current_user.id)
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['email', 'password']):
+        return jsonify({'error': 'Bad request'})
+    d = request.json
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.email == d['email']).first()
+    if user and user.check_password(d['password']):
+        login_user(user)
+        namecur = user.id
+        # print(namecur)
+        print(1000)
+        return jsonify({'success': 'OK'})
+    else:
+        return jsonify({'error': 'Wrong password'})
 
 
 @blueprint.route('/api/comments')
@@ -39,33 +67,39 @@ def get_one_news(news_id):
 
 @blueprint.route('/api/comments', methods=['POST'])
 def create_news():
-    if not request.json:
-        return jsonify({'error': 'Empty request'})
-    elif not all(key in request.json for key in
-                 ['content']):
-        return jsonify({'error': 'Bad request'})
-    db_sess = db_session.create_session()
-    news = News(
-        content=request.json['content'],
-        usid=current_user.id
-    )
-    db_sess.add(news)
-    db_sess.commit()
-    return jsonify({'success': 'OK'})
+    global namecur
+    try:
+        if not request.json:
+            return jsonify({'error': 'Empty request'})
+        elif not all(key in request.json for key in
+                     ['content']):
+            return jsonify({'error': 'Bad request'})
+        db_sess = db_session.create_session()
+        news = News(
+            content=request.json['content'],
+            usid=namecur
+        )
+        db_sess.add(news)
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
+    except Exception:
+        return jsonify({'error': 'please log in'})
 
 
 @blueprint.route('/api/comments/<int:news_id>', methods=['DELETE'])
 @blueprint.route('/api/commentsdel/<int:news_id>')
 def delete_news(news_id):
-    db_sess = db_session.create_session()
-    news = db_sess.query(News).get(news_id)
-    if news.usid == current_user.id:
-        if not news:
-            return jsonify({'error': 'Not found'})
-        db_sess.delete(news)
-        db_sess.commit()
-        return redirect("/")
-    else:
-        return jsonify({'error': 'It is not your comment'})
-
-
+    global namecur
+    try:
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).get(news_id)
+        if news.usid == namecur:
+            if not news:
+                return jsonify({'error': 'Not found'})
+            db_sess.delete(news)
+            db_sess.commit()
+            return redirect("/")
+        else:
+            return jsonify({'error': 'It is not your comment'})
+    except Exception:
+        return jsonify({'error': 'Please log in'})
