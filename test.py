c29356b8ae47c34ddae778af22d89dcaadc5c955
Egornak <egor.nak@bk.ru>
2021-04-19 03:03:38 +0300

    hi

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from requests import get, post, delete
 #
 # print(post('http://localhost:5000/api/news',
 #            json={'title': 'Заголовок'}).json())
-
-# print(delete('http://localhost:5000/api/news/1').json())
+print(post('http://localhost:5000/api/login',
+           json={'password': '12345678', 'email': 'egor.nak@bk.ru'}).json())
+print(delete('http://localhost:5000/api/comments/3').json())
 # print(delete('http://localhost:5000/api/news/999').json())
